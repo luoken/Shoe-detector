0c07ebf186f4cec3d346383483ad510ab2e48bc9
Jose <japaric4@calstatela.edu>
2018-03-01 12:17:26 -0800

    ready for use with zappos

--- scraper.py
+++ scraper.py
@@ -1,4 +1,6 @@
 import requests
+import lxml
+import urllib
 from bs4 import BeautifulSoup
 
 adidasUrl = "https://www.zappos.com/adidas-men-shoes/CK_XAVoBAcABAuICAwELGA.zso"
@@ -6,10 +8,26 @@ nikeUrl = "https://www.zappos.com/nike-men-shoes/CK_XAVoBb8ABAuICAwELGA.zso"
 vansUrl =   "https://www.zappos.com/vans-men-shoes/CK_XAVoCqwHAAQLiAgMBCxg.zso"
 
 
-def scrape(url):
-  page = requests.get(url)
-  soup = BeautifulSoup(page, 'lxml')
+def saveImg(url, brand, n):
+  name = brand + '-'  +  str(n) + '.jpg'
+  urllib.urlretrieve(url, name)
 
-  print(soup);
+def scrape(url):
+  r = requests.get(url)
+  imgSrcUrls = []
+
+  if(r.status_code == 200):
+    soup = BeautifulSoup(r.content, "lxml")
+    imgSrcList = soup.find_all("img", class_='productImg')
+    srcList = []
+
+    for i in imgSrcList:
+      srcList.append(i.get('src'))
+
+    for i in range(10):
+      if "http" in srcList[i]:
+        saveImg(srcList[i], 'adidas', i)
+  else:
+    print("Error: Response Status Code != 200")
 
 scrape(adidasUrl)
