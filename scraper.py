6bd0e2ce0d227aeb5ae1ebdfe4ed6487f9008777
Jose <japaric4@calstatela.edu>
2018-03-01 12:29:44 -0800

    brand name update

--- scraper.py
+++ scraper.py
@@ -7,7 +7,6 @@ adidasUrl = "https://www.zappos.com/adidas-men-shoes/CK_XAVoBAcABAuICAwELGA.zso"
 nikeUrl = "https://www.zappos.com/nike-men-shoes/CK_XAVoBb8ABAuICAwELGA.zso"
 vansUrl =   "https://www.zappos.com/vans-men-shoes/CK_XAVoCqwHAAQLiAgMBCxg.zso"
 
-
 def saveImg(url, brand, n):
   name = brand + '-'  +  str(n) + '.jpg'
   urllib.urlretrieve(url, name)
@@ -25,9 +24,17 @@ def scrape(url):
       srcList.append(i.get('src'))
 
     for i in range(10):
+      brandName = ''
       if "http" in srcList[i]:
-        saveImg(srcList[i], 'adidas', i)
+        if 'adidas' in url:
+          brandName = 'adidas'
+        elif 'nike' in url:
+          brandName = 'nike'
+        else:
+          brandName = 'vans'
+        saveImg(srcList[i], brandName, i)
   else:
     print("Error: Response Status Code != 200")
 
-scrape(adidasUrl)
+# change url and run
+scrape(vansUrl)
