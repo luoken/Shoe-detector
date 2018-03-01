65e10fd125b48710f1d60a9fac6c443969333911
Jose <japaric4@calstatela.edu>
2018-03-01 12:35:58 -0800

    fixes indentation

--- scraper.py
+++ scraper.py
@@ -3,6 +3,7 @@ import lxml
 import urllib
 from bs4 import BeautifulSoup
 
+
 adidasUrl = "https://www.zappos.com/adidas-men-shoes/CK_XAVoBAcABAuICAwELGA.zso"
 nikeUrl = "https://www.zappos.com/nike-men-shoes/CK_XAVoBb8ABAuICAwELGA.zso"
 vansUrl =   "https://www.zappos.com/vans-men-shoes/CK_XAVoCqwHAAQLiAgMBCxg.zso"
