c8c0e45e3a415be3d933f1407a28b6f5a5cb1784
Jose <japaric4@calstatela.edu>
2018-03-02 13:20:15 -0800

    saves images from product page

--- jose/scraper.py
+++ jose/scraper.py
@@ -3,19 +3,16 @@ import lxml
 import urllib
 from bs4 import BeautifulSoup
 
-# https://www.zappos.com/vans-men-shoes/CK_XAVoCqwHAAQLiAgMBCxg.zso?p=0
-
 
 adidas = "https://www.zappos.com/adidas-men-shoes/CK_XAVoBAcABAuICAwELGA.zso"
 nike = "https://www.zappos.com/nike-men-shoes/CK_XAVoBb8ABAuICAwELGA.zso"
 vans =   "https://www.zappos.com/vans-men-shoes/CK_XAVoCqwHAAQLiAgMBCxg.zso"
 
-def saveImg(url, brand, n):
-  name = brand + '-'  +  str(n) + '.jpg'
+def saveImg(url, name):
   urllib.urlretrieve(url, name)
 
 
-def scrape(url):
+def scrape(url, limit):
   r = requests.get(url)
   productLinks = []
   pageList = []
@@ -27,45 +24,47 @@ def scrape(url):
     for i in productLinks:
       pageList.append(i.get('href'))
 
-    for i in range(50):
-      if 'adidas' in url:
-        brandName = 'adidas'
-      elif 'nike' in url:
-        brandName = 'nike'
-      else:
-        brandName = 'vans'
+    if 'adidas' in url:
+      brandName = 'adidas'
+    elif 'nike' in url:
+      brandName = 'nike'
+    else:
+      brandName = 'vans'
 
-    for i in pageList:
-      # scrape productPage with url
-      # print("https://www.zappos.com" + i)
-      print('-------------------------------------------------------------------------')
-      scrapeProductPage("https://www.zappos.com" + i)
-      print('-------------------------------------------------------------------------')
-
-    print(len(pageList))
-
+    # for i in range(len(pageList)):
+    for i in range(limit):
+      url = pageList[i]
+      scrapeProductPage("https://www.zappos.com" + url, brandName, i)
   else:
     print("Error: Response Status Code != 200")
     print("Error: Ask Jose why it broke.")
 
 
-def scrapeProductPage(url):
-  print(url)
+def scrapeProductPage(url, brandName, i):
   r = requests.get(url)
-  # only want 3, 5
+
   if(r.status_code == 200):
     soup = BeautifulSoup(r.content, "lxml")
     soup = soup.find("div", id="thumbnailsList")
     soup = soup.find_all('a', class_="_1B_yd")
 
-    print(soup[3].get("href"))
-    print(soup[5].get("href"))
+    saveImg(soup[0].get("href"), brandName + "-both-" + str(i) + ".jpg")
+    saveImg(soup[3].get("href"), brandName + "-outside-"+ str(i) + ".jpg")
+    saveImg(soup[5].get("href"), brandName + "-inside-" + str(i) + ".jpg")
   else:
     print("Request to Product Page != 200")
 
 
-
-
 # change url and run
-scrape(vans)
-# 
+def scrapeZappos(n, limit):
+  for i in range(n):
+    print("Scraping from: " + vans + "?p=" + str(i))
+    scrape(vans + "?p=" + str(i), limit)
+  for i in range(n):
+    print("Scraping from: " + adidas + "?p=" + str(i))
+    scrape(adidas + "?p=" + str(i), limit)
+  for i in range(n):
+    print("Scraping from: " + nike + "?p=" + str(i))
+    scrape(nike + "?p=" + str(i), limit)
+# (# pages, # shoes per page)
+scrapeZappos(1, 10);
