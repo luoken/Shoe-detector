b4c168e823470beeedc2f42590e79f1826319354
Jose <japaric4@calstatela.edu>
2018-04-30 21:06:24 -0700

    update csv script and labels.csv

--- images/csv_script.py
+++ images/csv_script.py
@@ -3,20 +3,22 @@ import csv
 
 label_names = []
 
-def build_list_label():
-    for dirName, subdirList, fileList in os.walk('./'):
-        for filename in fileList:
-            if ".jpg" in filename:
-                label_name = dirName[1:] + "/" + filename
-                label_names.append(label_name)
-
 def update_csv_file():
-    build_list_label()
-
     with open('labels.csv', 'w', newline='') as csvfile:
         writer = csv.writer(csvfile, delimiter=',')
-        for label_name in label_names:
-            writer.writerow([label_name] + ["1"])
+        writer.writerow(["Path"] + ["Label"])
+    
+        for dirName, subdirList, fileList in os.walk('./'):
+            for filename in fileList:
+                label_name = dirName[1:] + "/" + filename
+                if "positive_images" in label_name:
+                    # writerow for 1
+                    label_name = dirName[1:] + "/" + filename
+                    writer.writerow([label_name] + ["1"])
+                elif "negative_images" in label_name:
+                    # writerow for 0
+                    label_name = dirName[1:] + "/" + filename
+                    writer.writerow([label_name] + ["0"])
 
 
 update_csv_file()
